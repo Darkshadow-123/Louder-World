913d23379b2a6ef5ab4ea9957b5b6c8255813ccc
Rishi Lalwani <rishi.123.lalwani@gmail.com>
2026-02-04 19:38:20 +0530

    Trigger Render rebuild with corrected API URL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Force Render rebuild - 2026-02-04
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
